e0591fe06bcbdc3a103d740b78e6351f31c4e8ce
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-03-26 12:35:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,7 @@ svn co --force https://github.com/coolsnowwolf/lede/trunk/package/lean package/l
 rm -Rf package/lean/qBittorrent/patches
 sed -i 's/PKG_SOURCE_URL:=.*/PKG_SOURCE_URL:=https:\/\/github.com\/c0re100\/qBittorrent-Enhanced-Edition/g' package/lean/qBittorrent/Makefile
 sed -i 's/PKG_HASH.*/PKG_SOURCE_PROTO:=git\nPKG_SOURCE_VERSION:=latest/g' package/lean/qBittorrent/Makefile
+sed -i '/PKG_BUILD_DIR/d' package/lean/qBittorrent/Makefile
 sed -i 's/+python$/+python3/g' package/lean/luci-app-qbittorrent/Makefile
 rm -Rf package/feeds/packages/php7
 svn co https://github.com/openwrt/packages/branches/openwrt-19.07/lang/php7 package/feeds/packages/php7
