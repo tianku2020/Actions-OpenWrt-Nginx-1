9e3f0397193bd10071f16e5529ded790d2549438
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-04-01 14:21:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -95,3 +95,4 @@ find package/feeds/*/ package/lean/ -maxdepth 3 -path "*luci-app*" -name "Makefi
 find package/feeds/*/ package/lean/ -maxdepth 3 -d -name "zh-cn" | xargs -i rename -v 's/zh-cn/zh_Hans/' {}
 find package/feeds/*/ package/lean/ -maxdepth 3 -name "Makefile" | xargs -i sed -i "/bin\/upx/d" {}
 find package/feeds/*/ package/lean/ -maxdepth 3 -name "Makefile" | xargs -i sed -i "s/po\/zh-cn/po\/zh_Hans/g" {}
+find package/feeds/*/ package/lean/ -maxdepth 3 -name "Makefile" | xargs -i sed -i "s/\+luci //g" {}
